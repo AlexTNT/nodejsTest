de82ee33af868fd3b0aba7cc53b98992fe348943
AlexTnt <dao_reunion@mail.ru>
2019-07-03 19:35:24 +0300

    initial commit

---
+++ src/js/server.js
@@ -0,0 +1,31 @@
+//server
+
+const http = require('http');
+const fs =   require('fs');
+
+
+const server = http.createServer(function(request, response){
+  console.log(request.method, request.url);
+  if( request.url === '/home' ) {
+    const html = fs.readFileSync('../index.html', 'utf8');
+    response.end(html);
+
+  } else if( request.url === '/main.css' ) {
+    const css = fs.readFileSync('../style/main.css', 'utf8');
+    response.end(css);
+
+  } else if( request.url === '/main.js' ) {
+    const js = fs.readFileSync('./main.js', 'utf8');
+    response.end(js);
+
+  } else if( request.url === '/') {
+    const error = fs.readFileSync('../404.html', 'utf8');
+    response.end(error);
+
+  } else {
+    response.end('');
+  }
+});
+
+server.listen(3000);
+console.log('server started');
