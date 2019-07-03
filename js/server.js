b47777b62824b8ff64638b76034ff9ef0b18f3f2
AlexTnt <dao_reunion@mail.ru>
2019-07-03 22:29:57 +0300

    reconfigure the project

--- js/server.js
+++ js/server.js
@@ -2,31 +2,32 @@
 
 const http = require('http');
 const fs =   require('fs');
-
+const port = process.env.PORT || 3000;
 
 const server = http.createServer(function(request, response){
   console.log(request.method, request.url);
   if( request.url === '/home' ) {
-    const html = fs.readFileSync('../index.html', 'utf8');
+    const html = fs.readFileSync('index.html', 'utf8');
     response.end(html);
 
   } else if( request.url === '/main.css' ) {
-    const css = fs.readFileSync('../style/main.css', 'utf8');
+    const css = fs.readFileSync('css/main.css', 'utf8');
     response.end(css);
 
   } else if( request.url === '/main.js' ) {
-    const js = fs.readFileSync('./main.js', 'utf8');
+    const js = fs.readFileSync('js/main.js', 'utf8');
     response.end(js);
 
   } else if( request.url === '/') {
-    const error = fs.readFileSync('../404.html', 'utf8');
+    const error = fs.readFileSync('404.html', 'utf8');
     response.end(error);
 
   } else {
-    const html = fs.readFileSync('../index.html', 'utf8');
+    const html = fs.readFileSync('index.html', 'utf8');
     response.end(html);
   }
 });
 
-server.listen(process.env.PORT || 3000);
+
+server.listen(port);
 console.log('server started');
